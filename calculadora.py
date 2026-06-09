d5bf1c8d499cd24b54772f384bdd9ae086aa1cf3
maurbinat <ma.urbinat@duocuc.cl>
2026-06-08 21:04:50 -0400

    Calculadora en python
    Calculadora con menú de opción sumar, restar, multiplicar y dividir.

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+print("-----Calculadora-------")
+num1 = int(input("Ingrese un número: "))
+num2 = int(input("Ingrese el otro numero: "))
+while True:
+    print("Menú de opcion")
+    print("1.- Sumar")
+    print("2.- Restar")
+    print("3.- Dividir")
+    print("4.- Multiplicar")
+    opc = int(input("Ingrese una opcion: "))
+    if opc == 1:
+        print("La suma es: " , num1 + num2)
+        break
+    if opc == 2:
+        print("La resta es: " , num1 + num2)
+        break
+    if opc == 3:
+        print("La division es: " , num1 / num2)
+        break
+    if opc == 4:
+        print("La multiplicacion es: " , num1 * num2)
+        break
